0bb02cbce9e7f5f7151228247bd1fe13d7821f56
MBA0200 <mba0200@MBA0200.local>
2020-08-03 08:31:12 +0700

    fix define deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3WlkHAKEGFFMi7YPLRa-oLivq8V_c7aU",
+    authDomain: "axamin-education.firebaseapp.com",
+    databaseURL: "https://axamin-education.firebaseio.com",
+    projectId: "axamin-education",
+    storageBucket: "axamin-education.appspot.com",
+    messagingSenderId: "824531092213",
+    appId: "1:824531092213:web:58aa0f4131451329f37bbc",
+    measurementId: "G-8S8NSVN26V"
+  }
 };
